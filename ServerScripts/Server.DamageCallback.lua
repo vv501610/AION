3948c1b0ab4f121344bdf3931969bc3a892c8456
GigaWifi <vv501610@gmail.com>
2023-12-14 14:17:32 +0900

    데미지 공식 수정(1.5/2)

--- ServerScripts/Server.DamageCallback.lua
+++ ServerScripts/Server.DamageCallback.lua
@@ -3,32 +3,36 @@ local DEF = 200 -- 방어상수
 local LUK_TO_CRI = 0.01 -- luk스탯 1당 크리티컬 확률 0.01% 상승
 local AGI_TO_DEAL = 2.5 -- agi스탯 1당 크리티컬 대미지 2.5% 상승
 local DEFAULT_CRI_DEAL = 20 -- 크리 터졌을 때 기본 20% 추가대미지
-local AMP = 12 -- 대미지 진폭 88 ~ 112
 
--- local statName = {[0]='def', [1]='magicDef'}
--- local skillType = {[-1]=0}
--- local index = 0
--- while Server.GetSkill(index) do
--- 	skillType[index] = Server.GetSkill(index).type
--- 	index = index+1
+
+-- local function reDmg(damage, defense)
+-- 	return damage * (1-defense / (defense+DEF))
 -- end
--- Server.GetSkill 에서 호출하는것보다 서버 시작시 테이블에 담아놓고 호출하는것이 월등히 빠름
-
-local function reDmg(damage, defense)
-	return damage * (1-defense / (defense+DEF))
-end
 
 Server.damageCallback = function(a, b, damage, skID, cri, visible)
-	if damage < 1 then -- 대미지가 1 미만일 때 대미지 미표시
-		return 0, false, false
+	if not a or not b then
+		return false
 	end
+
+	-- if damage < 1 then -- 대미지가 1 미만일 때 대미지 미표시
+	-- 	return 0, false, false
+	-- end
+
+	local bdef = b.def
+
+	print(a.GetStat(109)) -- 방어력 관통률
+
+	damage = damage*math.ceil(1000*(1-bdef/(bdef+15000)))/1000 ---뒤에 15000은 방어 상수
+
+	print((math.floor(bdef/(bdef+15000)*100*10)/10)..' %피해를 덜 받습니다.')
+
 	
-	damage = reDmg(damage, b.def)
+	-- damage = reDmg(damage, b.def)
 	
-	if rand(1, 100/LUK_TO_CRI+1) <= a.luk then
-		damage = damage * (1+DEFAULT_CRI_DEAL*0.01 + a.agi*AGI_TO_DEAL*0.01)
-		cri = true
-	end
+	-- if rand(1, 100/LUK_TO_CRI+1) <= a.luk then
+	-- 	damage = damage * (1+DEFAULT_CRI_DEAL*0.01 + a.agi*AGI_TO_DEAL*0.01)
+	-- 	cri = true
+	-- end
 	
 	-- damage = damage*rand(100-AMP, 101+AMP)*0.01
 
--- ServerScripts/Server.Refresh.lua
+++ ServerScripts/Server.Refresh.lua
@@ -1,26 +1,19 @@
 local function UnitStatsUpdata(unit)
 
-    local def = unit.def
+    -- local def = unit.def
+
+    -- local defs = 15000 -- 방어 상수
 
 
-    -- def = 310000
+    -- unit.customData.DamageReductionRate = math.ceil(1000*(1-def/(def+defs)))/1000
 
-    defs = 100000
+    -- print((math.floor(def/(def+defs)*100*10)/10)..' %피해를 덜 받습니다.')
 
---print( ((def)/(def + 200)) * 100)
-
-
-unit.customData.DamageReductionRate = math.ceil(1000*(1-def/(def+defs)))/1000
-
-
-
-
-print((math.floor(def/(def+defs)*100*10)/10)..' %피해를 덜 받습니다.')
-
-print(unit.customData.DamageReductionRate)
+    -- print(unit.customData.DamageReductionRate..'커스텀 스텟')
 
 end
 
 
 
 Server.onRefreshStats.Add(UnitStatsUpdata)
+
