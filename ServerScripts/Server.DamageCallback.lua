810fde9b5be7241416b2a519e4ff47e2b91cbd1f
GigaWifi <vv501610@gmail.com>
2023-12-21 00:41:11 +0900

    치명타확률, 데미지 추가

--- ServerScripts/Server.DamageCallback.lua
+++ ServerScripts/Server.DamageCallback.lua
@@ -1,7 +1,7 @@
 
 local DEF = 200 -- 방어상수
-local LUK_TO_CRI = 0.01 -- luk스탯 1당 크리티컬 확률 0.01% 상승
-local AGI_TO_DEAL = 2.5 -- agi스탯 1당 크리티컬 대미지 2.5% 상승
+
+
 local DEFAULT_CRI_DEAL = 20 -- 크리 터졌을 때 기본 20% 추가대미지
 
 
@@ -20,12 +20,30 @@ Server.damageCallback = function(a, b, damage, skID, cri, visible)
  
 	local bdef = b.def
 	-- print(a.GetStat(109)/1000) -- 방어력 관통률
-	local penetration_rate = a.GetStat(109)/1000-- 방어력 관통률 10%
+	local penetration_rate = a.GetStat(109)/1000-- 방어력 관통률 10%y
 	local abs_defense = b.GetStat(108) -- 절대 방어력 값
 	-- 방어력에서 방어력 관통률을 적용
 	local effective_bdef = bdef * (1 - penetration_rate)
 	
 	damage = damage*math.ceil(1000*(1-effective_bdef/(effective_bdef+15000)))/1000
+
+
+
+	local LUK_TO_CRI = b.GetStat(102) -- luk스탯 1당 크리티컬 확률 0.01% 상승
+	local AGI_TO_DEAL = b.GetStat(103)  -- agi스탯 1당 크리티컬 대미지 2.5% 상승
+	
+
+
+
+	if rand(1, 100/LUK_TO_CRI+1) <= a.luk then
+		damage = damage * (1+DEFAULT_CRI_DEAL*0.01 + a.agi*AGI_TO_DEAL*0.01)
+		cri = true
+	end
+
+
+
+
+	print(damage)
 
 		-- 절대 방어력 적용
 	damage = damage - abs_defense
@@ -36,7 +54,8 @@ Server.damageCallback = function(a, b, damage, skID, cri, visible)
 	end
 	
 
-	
+
+
 	-- print((math.floor(effective_bdef/(effective_bdef+15000)*100*10)/10)..' %피해를 덜 받습니다.')
 	
 	
